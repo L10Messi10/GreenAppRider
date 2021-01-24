942939f6cde1596a70f9d088702cf71bd8d3267d
L10Messi10 <loveyourenemy@outlook.com>
2021-01-24 13:18:46 +0800

    Add project files.

---
+++ GreenAppRider/GreenAppRider/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using GreenAppRider.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace GreenAppRider.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ GreenAppRider/GreenAppRider/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using GreenAppRider.Models;
+using GreenAppRider.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GreenAppRider.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
